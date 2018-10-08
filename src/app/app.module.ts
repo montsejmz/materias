07e9559c2bd4e90f782d128ef0015c0c8c93681f
alumno Secundaria <al3201@cedi.local>
2018-10-08 09:30:47 -0500

    Se agrego la pagina calculo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { HistoriaPage } from '../pages/historia/historia';
+import { CalculoPage } from '../pages/calculo/calculo';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    HistoriaPage
+    HistoriaPage,
+    CalculoPage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { HistoriaPage } from '../pages/historia/historia';
   entryComponents: [
     MyApp,
     HomePage,
-    HistoriaPage
+    HistoriaPage,
+    CalculoPage
   ],
   providers: [
     StatusBar,
